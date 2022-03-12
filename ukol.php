9c58e63846d27599c9bf9c9ac3573ac4b0f3c687
drda4 <petr.arthas.drda@gmail.com>
2022-03-12 17:17:26 +0100

    First version

---
+++ ukol.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<h1>Upload obrázku</h1>
+</head>
+<body>
+	<form action="ukol.php" method="post" enctype="multipart/form-data">
+		Vyberte soubor: <input type="file" name="soubor"><br>
+		Nový název obrázku: <input type="text" name="nazev"><br>
+		<input type="checkbox" name="vybrat"> Po nahrání zobrazit?<br>
+		<input type="submit" value="Nahrát">
+	</form>
+</body>	
+<?php
+
+
+
+
+
+
+?>
+</html>	
